1ce7a0230d7a56091468f299977d66f17c89bb2b
Calvodiscord <netgato786@gmail.com>
2025-11-20 09:31:33 -0300

    Create authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (user) return res.status(400).json({ msg: 'Usuário já existe' });
+
+        const isAdmin = email === 'calvoeditofc@gmail.com';
+
+        user = new User({ username, email, password, isAdmin });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = { user: { id: user.id, isAdmin: user.isAdmin } };
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro no servidor');
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ msg: 'Credenciais inválidas' });
+
+        if (user.isBanned) return res.status(403).json({ msg: 'Esta conta foi banida.' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: 'Credenciais inválidas' });
+
+        const payload = { user: { id: user.id, isAdmin: user.isAdmin } };
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro no servidor');
+    }
+};
